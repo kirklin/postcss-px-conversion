37589213ff25784ff4f7ccf0b1f9c09109528830
Kirk Lin <linkirk@163.com>
2023-11-14 19:28:17 +0800

    fix: remove cjs export

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,4 @@ import { plugin } from "./pluginFactory";
 import type { Options } from "./types";
 
 const postcssPlugin = (opts: Partial<Options>) => plugin(opts);
-// eslint-disable-next-line kirklin/no-cjs-exports
-module.exports = postcssPlugin;
-// eslint-disable-next-line kirklin/no-cjs-exports
-module.exports.postcss = true;
 export default postcssPlugin;
